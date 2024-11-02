cedaf7609bd152eccb82dfdff7a24afc58dccb0c
VonEquinox <VonCrystalOrchid@outlook.com>
2024-11-02 16:40:02 +0800

    Update c5-3-1.c

--- Experience5/c5-3-1.c
+++ Experience5/c5-3-1.c
@@ -10,7 +10,8 @@ int main()
     fp = fopen("score.txt", "r");
     fp_out = fopen("score_avg.txt", "w");
     fprintf(fp_out, "学号\t高数\t英语\tC语言\t平均分\n");
-    while (fscanf(fp, "%d %d %d %d", &id, &math, &english, &c_language) != EOF) {
+    while (!feof(fp)) {
+        fscanf(fp, "%d %d %d %d", &id, &math, &english, &c_language);
         avg = (math + english + c_language) / 3.0;
         fprintf(fp_out, "%d\t%d\t%d\t%d\t%.2f\n", id, math, english, c_language, avg);
         printf("%d\t%d\t%d\t%d\t%.2f\n", id, math, english, c_language, avg);
