53a047d5173c79ae6e5acb4a95eed6e7b697029b
VonEquinox <VonCrystalOrchid@outlook.com>
2024-11-05 08:34:52 +0800

    Update c5-3-2(without array).c

--- Experience5/c5-3-2(without array).c
+++ Experience5/c5-3-2(without array).c
@@ -1,22 +1,30 @@
-/*c5-3-2.c the best version i think*/
+/*c5-3-2.c*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-int main() {
+int main()
+ {
+ 	FILE*fp;
     int Count = 0;
     double TotalAmount = 0;
+    fp=fopen("red_packet.txt", "w");
     printf("请输入红包总金额: ");
     scanf("%lf", &TotalAmount);
+    fprintf(fp,"红包总金额为%lf元\n",TotalAmount);
     printf("请输入红包个数: ");
     scanf("%d", &Count);
+    fprintf(fp,"红包的个数为%d个\n",Count);
     unsigned int seed = (unsigned int)time(NULL);
     srand(seed);
     long long sum = 0;
     for (int i = 0; i < Count; i++)
         sum += rand();
     srand(seed);
-    for (int i = 0; i < Count; i++)
-        printf("%lf\n",TotalAmount * (double)rand() / sum);
+    for (int i = 0; i < Count; i++) {
+        double tempd = (double)rand();
+        printf("%lf\n",TotalAmount * tempd / sum);
+        fprintf(fp,"%lf\n",TotalAmount *  tempd / sum);
+    }
+    fclose(fp);
     return 0;
 }
