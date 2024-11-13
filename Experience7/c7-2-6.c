3ff6896ffed19781607714ee85a0b9267a77457d
VonEquinox <VonCrystalOrchid@outlook.com>
2024-11-13 10:43:12 +0800

    Update c7-2-6.c

--- Experience7/c7-2-6.c
+++ Experience7/c7-2-6.c
@@ -3,11 +3,10 @@
 int main()
 {
     int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int i, sum = 0, b;
-    for (i = 0; i < 10; i++) {
-        if (i % 2 == 0)
-            sum += a[i];
-    }
-    printf("%d", sum);
+    int sum = 0;
+    int b = 0;
+    for (int i = 0; i < 10; i+=2)
+        sum += a[i];
+    printf("%d\n", sum);
     return 0;
 }
