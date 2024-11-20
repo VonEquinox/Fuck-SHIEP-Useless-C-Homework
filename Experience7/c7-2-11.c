258a80323eb2a0da0488a91a2aac42d9867031ac
VonEquinox <VonCrystalOrchid@outlook.com>
2024-11-20 09:10:57 +0800

    Update c7-2-11.c

--- Experience7/c7-2-11.c
+++ Experience7/c7-2-11.c
@@ -4,19 +4,15 @@
 
 int main()
 {
-    int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, i = 0, j = 0;
-    float h = 0.0;
     int arr[6] = {0};
     srand((unsigned int)time(NULL));
     for (i = 0; i < 6000; i++) {
-        j = 6*(double)rand() /RAND_MAX;
-        arr[j]++;
+        int j = 6.0*((double)rand() /RAND_MAX) + 1;
+        arr[j - 1]++;
     }
     for (i = 0; i < 6; i++) {
-        h = arr[i];
-        h = h / 6000;
         printf("数字%d出现次数：%d\n", i, arr[i]);
-        printf("数字%d出现概率：%f\n", i, h);
+        printf("数字%d出现概率：%f\n", i, arr[i] / 6000);
     }
     return 0;
 }
