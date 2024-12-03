48287edfb619e5cb38361dedd18222745549e29b
VonEquinox <VonCrystalOrchid@outlook.com>
2024-12-03 08:04:14 +0800

    Update c9-3-1.c

--- Experience9/c9-3-1.c
+++ Experience9/c9-3-1.c
@@ -1,58 +1,68 @@
-/*c9-3-1.c*/
+#include <cstdio>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct students 
-{
-    int id;
-    char name[80];
-    int math;
-    int eng;
-    int C;
-    float avg;
+
+#define ARRAY_LENGTH(Array) (sizeof(Array)/sizeof(Array[0]))
+
+struct StudentScore {
+    int Number;
+    char Name[20];
+    int MathScore;
+    int EnglishScore;
+    int CScore;
 };
 
-int compare(const void* elem1, const void* elem2)
-{
-    return ((struct students*)elem2)->avg - ((struct students*)elem1)->avg;
-}
+struct StudentAverageScore {
+    int Number;
+    char Name[20];
+    int MathScore;
+    int EnglishScore;
+    int CScore;
+    double AverageScore;
+};
 
 int main()
 {
-    FILE* inputfp, * outputfp;
-    struct students num[80];//声明结构体变量
-    int count = 0;
-    int i;
-    char line[100];//储存一行字符的数组
-    inputfp = fopen("score.txt", "r");
-    outputfp = fopen("score_avg.txt", "w");
-    if (inputfp == NULL || outputfp == NULL)
-    {
-        printf("error!!!!!");
-        return 1;
+    StudentScore StudentData[5] = {};
+    FILE *rfp = fopen("score.txt", "r");
+    //读取
+    int Index = 0;
+    while (fscanf(rfp,
+    "%d %s %d %d %d",
+    &StudentData[Index].Number,
+    StudentData[Index].Name,
+    &StudentData[Index].MathScore,
+    &StudentData[Index].EnglishScore,
+    &StudentData[Index].CScore) != EOF)
+        Index++;
+    fclose(rfp);
+    //求平均值
+    StudentAverageScore SAVData[5] = {};
+    for (int i = 0; i < ARRAY_LENGTH(StudentData); i++) {
+        SAVData[i].Number = StudentData[i].Number;
+        //把一个字符串赋值为另一个需要用strcpy
+        strcpy(SAVData[i].Name, StudentData[i].Name);
+        SAVData[i].AverageScore = ((double)StudentData[i].MathScore + (double)StudentData[i].EnglishScore + (double)StudentData[i].CScore) / 3;
+        SAVData[i].MathScore = StudentData[i].MathScore;
+        SAVData[i].EnglishScore = StudentData[i].EnglishScore;
+        SAVData[i].CScore = StudentData[i].CScore;
     }
-    while (fgets(line, sizeof(line), inputfp))//(储存位置，储存数据大小，输入方式（stdin为标准输入，即键盘输入))
-    {
-        
-        num[count].id = atoi(strtok(line, " "));//学号字符串转化为整数，以空格为分隔符拆分字符串，line是指针指向要分割的字符串。
-        strcpy(num[count].name , strtok(NULL, " "));//将名字拷贝至name结构体。strock发现分隔符后会将分隔符变为\0字符，返回NULL空指针，故第一次调用时要指明位置，之后调用使用空指针
-        num[count].math = atoi(strtok(NULL, " "));
-        num[count].eng = atoi(strtok(NULL," "));
-        num[count].C = atoi(strtok(NULL," "));
-        num[count].avg = (num[count].math + num[count].eng + num[count].C) / 3.0;
-        count++;
+    //排序
+    for (int i = 0; i < ARRAY_LENGTH(SAVData); i++) {
+        for (int j = 0; j < ARRAY_LENGTH(SAVData) - 1; j++) {
+            if (SAVData[j].AverageScore < SAVData[j+1].AverageScore){
+                StudentAverageScore Temp = SAVData[j];
+                SAVData[j] = SAVData[j+1];
+                SAVData[j+1] = Temp;
+            }
+        }
     }
-    qsort(num, count, sizeof(struct students), compare);//快速排序函数    
-    printf( "学号\t姓名\t高数\t英语\tC语言\t平均分\n");
-    fprintf(outputfp, "学号\t姓名\t高数\t英语\tC语言\t平均分\n");
-    for (i = 0; i < count; i++)
-    {
-        fprintf(outputfp,"%d\t%s\t%d\t%d\t%d\t% .2f\n", num[i].id, num[i].name, num[i].math, num[i].eng, num[i].C, num[i].avg);
-        printf("%d\t%s\t%d\t%d\t%d\t% .2f\n", num[i].id, num[i].name, num[i].math, num[i].eng, num[i].C, num[i].avg);
-    }
-    fclose(inputfp);
-    fclose(outputfp);
-    inputfp = NULL;
-    outputfp = NULL;
+    //输出
+    FILE* wfp = fopen("score_avg.txt", "w");
+    fprintf(wfp, "学号\t姓名\t高数\t英语\tC语言\t平均分\n");
+    for (int i = 0; i < ARRAY_LENGTH(SAVData); i++)
+        fprintf(wfp, "%d\t%s\t%d\t%d\t%d\t%.2lf\n", SAVData[i].Number, SAVData[i].Name, SAVData[i].MathScore, SAVData[i].EnglishScore, SAVData[i].CScore,SAVData[i].AverageScore);
+    fclose(wfp);
     return 0;
 }
