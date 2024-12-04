00bd5966a29ce62d63ce84346f1c21bd47d0e9fc
VonEquinox <VonCrystalOrchid@outlook.com>
2024-12-04 10:43:11 +0800

    Create c10-1-1.c

---
+++ Experience10/c10-1-1.c
@@ -0,0 +1,13 @@
+/*c10-1-1.c*/
+#include <stdio.h>
+int main()
+{
+	int m, n, t;
+	printf("input the m and n:");
+	scanf("%d %d", &m, &n);
+	t = m;
+	m = n;
+	n = t;
+	printf("m = %d,n = %d", m, n);
+	return 0;
+}
