932b99e48af9c80223d3e309fbc4c23f174566f3
VonEquinox <VonCrystalOrchid@outlook.com>
2024-12-23 10:25:44 +0800

    add experience 12

--- Experience12/c12-2-4.c
+++ Experience12/c12-2-4.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 int amax(int *a, int n)
 {
-	int max = 0, i = 0;
-	max = *a;
-	for (i = 1; i < n; i++)
-		if (max < *(a + i))
-			max = *(a + i);
-	return max;
+    int max = 0, i = 0;
+    max = *a;
+    for (i = 1; i < n; i++)
+        if (max < *(a + i))
+            max = *(a + i);
+    return max;
 }
 int main()
 {
-	int a[10];
-	int i = 0;
-	printf("input array a:");
-	for (i = 0; i < 10; i++)
-		scanf("%d", &a[i]);
-	printf("MAX = %d\n", amax(a, 10));
-	return 0;
+    int a[10];
+    int i = 0;
+    printf("input array a:");
+    for (i = 0; i < 10; i++)
+        scanf("%d", &a[i]);
+    printf("MAX = %d\n", amax(a, 10));
+    return 0;
 }
